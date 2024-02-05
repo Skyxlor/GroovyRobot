da619ada97750e8cac797178814e94576eec6e21
Sкყ x ℓσαя∂ <157386594+Skyxlor@users.noreply.github.com>
2024-02-05 04:10:38 -0800

    Add files via upload

---
+++ GroovyRobot/modules/source.py
@@ -0,0 +1,50 @@
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from GroovyRobot import OWNER_ID, dispatcher
+from GroovyRobot import pbot as client
+
+Groovy = "https://graph.org/file/77a310c89c7b9cd5ee2c7.jpg"
+
+
+@client.on_message(filters.command(["repo", "source"]))
+async def repo(client, message):
+    await message.reply_photo(
+        photo=Groovy,
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [ʀσᴄᴋყ](tg://user?id={OWNER_ID})
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+
+**✘ᴅ Rᴏʙᴏᴛ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴏᴡɴᴇʀ ",
+                        url="https://t.me/M3_4_U"
+                    ),
+                    InlineKeyboardButton(
+                        "• ʀᴇᴘᴏ •",
+                        url="https://t.me/N91Ab/6",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+
+__mod_name__ = "Rᴇᴩᴏ"
+_help__ = """
+ /repo  ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ 
+ /source ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ
+"""
